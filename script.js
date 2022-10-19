c5a35862e0e82cb5559786981e895f3596a9b7b7
VincenzoZ24 <zitov287@gmail.com>
2022-10-19 14:49:30 +0200

    generatore password completato

--- script.js
+++ script.js
@@ -1,2 +1,7 @@
-let nome = prompt("Come ti chiami?") 
-let cognome = prompt("Qual è il tuo cognome?") 
+let nome = prompt("Come ti chiami?");
+let cognome = prompt("Qual è il tuo cognome?");
+let annoDiNascita = prompt('Quando sei nata');
+let eta = 2022 - annoDiNascita;
+let colorePreferito = prompt("Colore preferito");
+
+document.getElementById('pwd').innerHTML = nome+cognome+eta+colorePreferito+'22';
